859d57627bd4b31958e81b66ae7f87f740e361a0
gabrielefarci <farci.gabriele2002@gmail.com>
2023-04-03 16:35:29 +0200

    aggiunta di un reindirizzamento

--- m1/s2/g1/js/index.js
+++ m1/s2/g1/js/index.js
@@ -12,8 +12,9 @@ function verifica() {
     if(anni >= 18) {
         document.getElementById('box2').innerHTML = 'Sei maggiorenne';
     }else {
-        document.getElementById('box2').innerHTML = 'Sei minorenne';
+        document.getElementById('box2').innerHTML = 'Sei minorenne. Tra 5 secondi verrai reindirizzato a una pagina più adatta';
+        setTimeout(function() {
+            window.location.href = "https://www.peppapig.com/it-it";
+        }, 5000);
     }
-
 }
-   
