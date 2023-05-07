94e7fa81b4ed34e5671646419a6c2f10efaebf08
gabrielefarci <farci.gabriele2002@gmail.com>
2023-05-07 21:15:25 +0200

    progetto s2 completato

--- m2/s2/progetto/script.js
+++ m2/s2/progetto/script.js
@@ -1,6 +1,20 @@
 // genres
 
+let buttonGenres = document.getElementById('btn-genres');
+buttonGenres.addEventListener('click', showHideGenres);
 
+let listGenres = document.getElementById('list-genres');
+listGenres.style.display = 'none';
+
+function showHideGenres() {
+
+    if(listGenres.style.display === 'none') {
+        listGenres.style.display = 'block';
+    } else {
+        listGenres.style.display = 'none';
+    }
+
+}
 
 // first slider
 
@@ -13,7 +27,21 @@ new Swiper('#swiper-1', {
       nextEl: '#next',
       prevEl: '#prev',
     },
-  
+    breakpoints: {
+        1400: {
+            slidesPerView: 6.2,
+        },
+        1100: {
+            slidesPerView: 5.2,
+        },
+        885: {
+            slidesPerView: 4.2,
+        },
+        350: {
+            slidesPerView: 2.2,
+        }
+    }
+
 })
 
 // second slider
@@ -27,6 +55,20 @@ new Swiper('#swiper-2', {
       nextEl: '#next2',
       prevEl: '#prev2',
     },
+    breakpoints: {
+        1400: {
+            slidesPerView: 6.2,
+        },
+        1100: {
+            slidesPerView: 5.2,
+        },
+        885: {
+            slidesPerView: 4.2,
+        },
+        350: {
+            slidesPerView: 2.2,
+        }
+    }
   
 })
 
@@ -41,6 +83,20 @@ new Swiper('#swiper-3', {
       nextEl: '#next3',
       prevEl: '#prev3',
     },
+    breakpoints: {
+        1400: {
+            slidesPerView: 6.2,
+        },
+        1100: {
+            slidesPerView: 5.2,
+        },
+        885: {
+            slidesPerView: 4.2,
+        },
+        350: {
+            slidesPerView: 2.2,
+        }
+    }
   
 })
 
@@ -51,4 +107,34 @@ window.onload = function() {
     let currentYear = document.getElementById('current-year');
     currentYear.innerText = new Date().getFullYear();
   
+}
+
+/* hero content color */
+
+let className = "inverted";
+let scrollTrigger = 60;
+
+window.onscroll = function() {
+  
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.querySelector('.hero-content').classList.add('active');
+  } else {
+    document.querySelector('.hero-content').classList.remove('active');
+  }
+};
+
+// show search area 
+
+let btnSearch = document.getElementById('search');
+btnSearch.addEventListener('click', showArea);
+
+let areaText = document.getElementById('input-search');
+areaText.style.display = 'none';
+
+function showArea() {
+    if(areaText.style.display == 'none') {
+        areaText.style.display = 'block';
+    } else {
+        areaText.style.display = 'none';
+    }
 }
