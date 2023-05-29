e819f4f69c639965911470fa44725872a5ca50fc
gabrielefarci <farci.gabriele2002@gmail.com>
2023-05-29 16:55:51 +0200

    primo esercizio con angular

---
+++ m3/s2/g1/esercizio/src/app/comp2/comp2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp2',
+  templateUrl: './comp2.component.html',
+  styleUrls: ['./comp2.component.scss'],
+  styles: ['p{text-align: center; color: red}']
+})
+export class Comp2Component {
+
+}
---
+++ m3/s2/g1/esercizio/src/app/comp3/comp3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp3',
+  templateUrl: './comp3.component.html',
+  styleUrls: ['./comp3.component.scss'],
+  styles: ['p{text-align: center; color: aqua}']
+})
+export class Comp3Component {
+
+}
