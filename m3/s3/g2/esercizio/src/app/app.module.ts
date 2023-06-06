1c198c01ea895875bb1f332489c92929f34870ef
gabrielefarci <farci.gabriele2002@gmail.com>
2023-06-07 00:35:20 +0200

    modifiche esercizio con i form

--- m3/s3/g2/esercizio/src/app/app.module.ts
+++ m3/s3/g2/esercizio/src/app/app.module.ts
@@ -1,12 +1,12 @@
+import { ReactiveFormComponent } from './pages/reactive-form/reactive-form.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormTemplateDrivenComponent } from './pages/form-template-driven/form-template-driven.component';
-import { ReactiveFormComponent } from './pages/reactive-form/reactive-form.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- m3/s3/g2/esercizio/src/app/pages/reactive-form/reactive-form.component.ts
+++ m3/s3/g2/esercizio/src/app/pages/reactive-form/reactive-form.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.scss']
 })
-export class ReactiveFormComponent {
+export class ReactiveFormComponent implements OnInit {
+
+  powers:string[] = ['Strength', 'Speed', 'Mind Control'];
+
+  form!: FormGroup;
+
+  constructor(private fb:FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: this.fb.control(null, Validators.required),
+      alterEgo: this.fb.control(null, Validators.required),
+      power: this.fb.control(null, Validators.required),
+      enemy: this.fb.control(null, [Validators.required, Validators.maxLength(10)]),
+      planet: this.fb.control(null, [Validators.required, Validators.minLength(5)]),
+      weekness: this.fb.control(null)
+    })
+  }
+
+  submit() {
+    console.log(this.form!.value);
+
+    this.form!.reset();
+  }
+
+  nameErrors(name:string) {
+    return this.form.get(name)?.invalid && this.form.get(name)?.touched;
+  }
+
+  alterEgoErrors(alterEgo:string) {
+    return this.form.get(alterEgo)?.invalid && this.form.get(alterEgo)?.touched;
+  }
+
+  powerErrors(power:string) {
+    return this.form.get(power)?.invalid && this.form.get(power)?.touched;
+  }
+
+  enemyErrors(enemy:string) {
+    return this.form.get(enemy)?.invalid && this.form.get(enemy)?.touched || this.form.get(enemy)?.errors?.['maxlength'];
+  }
+
+  planetErrors(planet:string) {
+    return this.form.get(planet)?.invalid && this.form.get(planet)?.touched || this.form.get(planet)?.errors?.['minlength'];
+  }
 
 }
